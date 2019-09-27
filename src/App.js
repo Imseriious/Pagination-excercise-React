eb4c2131cdcfef4ab3c5ee88c120423ca35ea0e1
Sammy.p <samueledi80@hotmail.com>
2019-09-27 11:22:36 +0200

    Links added to pagination

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage] = useState(10);
 
   useEffect(() => { //Don't use async on useEffect
     const fetchPosts = async () => { //use async here
@@ -26,11 +26,14 @@ const App = () => {
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
   
+  //Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
   return (
     <div className="container mt-5">
       <h1 className="text-primaty-mb-3">My Blog</h1>
       <Posts posts={currentPosts} loading={loading} />
-      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length}/>
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>
     </div>
   );
 }
