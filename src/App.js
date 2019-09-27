4ebbffef7e0613c8e726da03164e71b5527c5365
Sammy.p <samueledi80@hotmail.com>
2019-09-27 10:48:33 +0200

    Fetch and display post titles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Posts from './components/Posts';
 
 const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage, setPostPerPage] = useState(10);
+
+  useEffect(() => { //Don't use async on useEffect
+    const fetchPosts = async () => { //use async here
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+
+    fetchPosts();
+  }, []); //Empty brakets after so it runs only when mount
+
+
+  
   return (
-    <div className="App">
-      <h1>My App</h1>
+    <div className="container mt-5">
+      <h1 className="text-primaty-mb-3">My Blog</h1>
+      <Posts posts={posts} loading={loading} />
     </div>
   );
 }
